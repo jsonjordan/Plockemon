0a58d4b57ce3042b24b3891fe100dad551e58e32
Jason Jordan <jtjordan13@gmail.com>
2016-06-10 15:40:08 -0400

    Added Heroku env token

--- app.rb
+++ app.rb
@@ -99,20 +99,20 @@ class PlockApp < Sinatra::Base
       halt 400, "Not all information provided"
     end
 
-    # text = "@#{user.username} recommended #{parsed_data["url"]} for @#{friend.username}!"
-    #
-    # HTTParty.post("https://slack.com/api/chat.postMessage",
-    #               query: {token: Token,
-    #                       channel: "C1FJJ2W1F",
-    #                       text: text,
-    #                       unfurl_links: true,
-    #                       unfurl_media: true,
-    #                       link_names: 1,
-    #                       username: "Plokebot",
-    #                       as_user: true,
-    #                       icon_url: "http://orig14.deviantart.net/bfb7/f/2014/221/2/4/pokeball_cutie_mark_by_cottenheart-d7ufe4x.png"
-    #                       }
-    #                       )
+    text = "@#{user.username} recommended #{parsed_data["url"]} for @#{friend.username}!"
+
+    HTTParty.post("https://slack.com/api/chat.postMessage",
+                  query: {token: SLACK_TOKEN,
+                          channel: "C1FJJ2W1F",
+                          text: text,
+                          unfurl_links: true,
+                          unfurl_media: true,
+                          link_names: 1,
+                          username: "Plokebot",
+                          as_user: true,
+                          icon_url: "http://orig14.deviantart.net/bfb7/f/2014/221/2/4/pokeball_cutie_mark_by_cottenheart-d7ufe4x.png"
+                          }
+                          )
   end
 
 end
