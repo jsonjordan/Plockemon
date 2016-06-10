ca4eb3b6e65f1062d208145f393f429ab07f3d2a
Jason Jordan <jtjordan13@gmail.com>
2016-06-10 11:50:20 -0400

    Test file updated

--- slackapitest.rb
+++ slackapitest.rb
@@ -3,16 +3,18 @@ require 'httparty'
 require 'json'
 require './token'
 
+text = "@mariacassino recommended https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet for @jason.jordan!"
 
 r = HTTParty.post("https://slack.com/api/chat.postMessage",
               query: {token: Token,
                       channel: "C1FJJ2W1F",
-                      text: "Icon test",
+                      text: text,
+                      unfurl_links: true,
+                      unfurl_media: true,
+                      link_names: 1,
                       username: "Plokebot",
-                      as_user: false,
+                      as_user: true,
                       icon_url: "http://orig14.deviantart.net/bfb7/f/2014/221/2/4/pokeball_cutie_mark_by_cottenheart-d7ufe4x.png"
                       }
                       )
-
-
-binding.pry
+#@mariacassino recommended for @jason.jordan:
