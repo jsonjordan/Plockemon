417d4f61c469f1822cdceb19a4a8aef52e9636a0
Jason Jordan <jtjordan13@gmail.com>
2016-06-10 11:25:22 -0400

    Formatted query for more readablilty

--- slackapitest.rb
+++ slackapitest.rb
@@ -4,7 +4,15 @@ require 'json'
 require './token'
 
 
-r = HTTParty.post("https://slack.com/api/chat.postMessage", query: {token: Token, channel: "C1FJJ2W1F", text: "Icon test", username: "Plokebot", as_user: false, icon_url: "http://orig14.deviantart.net/bfb7/f/2014/221/2/4/pokeball_cutie_mark_by_cottenheart-d7ufe4x.png"})
+r = HTTParty.post("https://slack.com/api/chat.postMessage",
+              query: {token: Token,
+                      channel: "C1FJJ2W1F",
+                      text: "Icon test",
+                      username: "Plokebot",
+                      as_user: false,
+                      icon_url: "http://orig14.deviantart.net/bfb7/f/2014/221/2/4/pokeball_cutie_mark_by_cottenheart-d7ufe4x.png"
+                      }
+                      )
 
 
 binding.pry
